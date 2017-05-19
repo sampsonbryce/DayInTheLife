8b3b9e03ec208723392b095ae66877187311d532
Bryce Sampson <sampson.bryce@yahoo.com>
2017-05-19 16:50:26 -0700

    auth working

--- server/app/routes/auth_routes.js
+++ server/app/routes/auth_routes.js
@@ -1,6 +1,24 @@
-module.exports = function(app, passport){
-    app.post('/signup', passport.authenticate('local-signup', {failureFlash: true}));
-    app.post('/login', passport.authenticate('local-login', {failureFlash: true}));
+module.exports = function (app, passport) {
+    app.post('/signup', passport.authenticate('local-signup', { failureFlash: true }));
+    app.post('/login', function (req, res, next) {
+        passport.authenticate('local-login', function (err, user, info) {
+            console.log('info', info)
+            if (err) { return next(err); }
+            if (!user) { 
+                console.log('returing user not found json');
+                res.status(401);
+                res.setHeader('Content-Type', 'application/json');
+                return res.send(JSON.stringify({ message: info.loginMessage }, null, 3));
+            }
+            req.logIn(user, function (err) {
+                if (err) { return next(err); }
+                console.log('returing failed to login json');
+                res.status(401);
+                res.setHeader('Content-Type', 'application/json');
+                return res.send(JSON.stringify({ message: "Failed to log in" }, null, 3));
+            });
+        })(req, res, next);
+    });
 
     function isLoggedIn(req, res, next) {
         console.log('is logged in?');
@@ -10,5 +28,5 @@ module.exports = function(app, passport){
 
         // if they aren't redirect them to the home page
         res.redirect('/');
-    }    
+    }
 }
